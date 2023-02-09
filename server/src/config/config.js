8483fb87f62ba4ae78beb5ad2aaeda208213f323
Adnan Ovcina <adnanovcina1987@gmail.com>
2023-02-09 10:14:01 +0100

    Update config.js

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 
 const password = process.env.PASSWORD
 const database = process.env.DATABASE
+const user = process.env.USER
 
 
 const config = {
     port: process.env.PORT || 5000,
     secret: process.env.JWT_SECRET || 'ay+5M9*85&B8W*zp',
-    mongo: process.env.MONGO || `mongodb+srv://aovcina:${password}@cluster0.hbhrs.mongodb.net/${database}?retryWrites=true&w=majority`
+    mongo: process.env.MONGO || `mongodb+srv://${user}:${password}@cluster0.hbhrs.mongodb.net/${database}?retryWrites=true&w=majority`
 }
 
 export default config;
